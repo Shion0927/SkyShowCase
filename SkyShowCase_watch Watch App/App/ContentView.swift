53d9bb4d3b8844efce0db1c5f3dafbb1cd588803
Shion Uemura <130330406+Shion0927@users.noreply.github.com>
2025-12-23 14:29:54 +0900

    watchOS用のUIを追加

---
+++ SkyShowCase_watch Watch App/App/ContentView.swift
@@ -0,0 +1,14 @@
+//
+//  ContentView.swift
+//  SkyShowCase_watch Watch App
+//
+//  Created by 植村詩苑 on 2025/12/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        WatchHomeView()
+    }
+}
